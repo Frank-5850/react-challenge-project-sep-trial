8ba38ee1d49834de70e7aa3a9c4243c80e72f1b6
Franco <phanfranco@yahoo.com>
2021-10-21 15:33:42 -0700

    feat(logOut): Logging out now clears the state values.

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import "./nav.css";
+import { logoutUser } from "../../redux/actions/authActions";
 
 const Nav = (props) => {
-    return (
-        <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
-                </div>
-            </Link>
-            <Link to={"/view-orders"} className="nav-link" id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <Link to={"/login"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </Link>
+  const dispatch = useDispatch();
+
+  return (
+    <div className="nav-strip">
+      <Link to={"/order"} className="nav-link">
+        <div className="nav-link-style">
+          <label className="nav-label">Order Form</label>
         </div>
-    );
-}
+      </Link>
+      <Link to={"/view-orders"} className="nav-link" id="middle-link">
+        <div className="nav-link-style">
+          <label className="nav-label">View Orders</label>
+        </div>
+      </Link>
+      <Link to={"/login"} className="nav-link">
+        <div className="nav-link-style" onClick={() => dispatch(logoutUser())}>
+          <label className="nav-label">Log Out</label>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default Nav;
