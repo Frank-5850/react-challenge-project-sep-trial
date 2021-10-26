b8036b37adec85ebfbbbaec48093b4dce0881c75
Franco <phanfranco@yahoo.com>
2021-10-26 14:27:55 -0700

    feat(PrivateRoutes): Users that are not logged in are unable to access certain routes.

--- application/src/components/login/login.js
+++ application/src/components/login/login.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import LoginForm from './login-form/loginForm';
-import './login.css';
+import React, { useEffect } from "react";
+import LoginForm from "./login-form/loginForm";
+import "./login.css";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 
-class Login extends Component {
-  
-  render() {
-    return (
-      <div className="main-body">
-        <h1 className="text-center">Login Screen</h1>
-        <div className="d-flex justify-content-center mt-5">
-          <LoginForm onLogin={() => {this.props.history.push('/view-orders')}}/>
-        </div>
+const Login = () => {
+  const auth = useSelector((state) => state.auth.token);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (auth) history.push("/view-orders");
+  }, [auth]);
+
+  return (
+    <div className="main-body">
+      <h1 className="text-center">Login Screen</h1>
+      <div className="d-flex justify-content-center mt-5">
+        <LoginForm
+          onLogin={() => {
+            history.push("/view-orders");
+          }}
+        />
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
---
+++ application/src/router/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const auth = useSelector((state) => state.auth.token);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return auth ? <Component {...props} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
